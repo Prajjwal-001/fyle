ccd71754f622be003ad01644e60f289fc1e15fd2
Prjjwal Singh <01prajsingh@gmail.com>
2024-06-03 23:38:39 +0530

    One last commit

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,19 @@
 export async function formSubmit(event) {
-    console.log('FORM submission received');
-    const formData = event.body;
-  
-    try {
-      console.log('Form data:', formData);
-      return {
-        statusCode: 200,
-        body: 'Form submitted successfully!',
-      };
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      return {
-        statusCode: 500,
-        body: 'Failed to submit form',
-      };
-    }
+  console.log('FORM submission received');
+  const request = event.request;
+  const formData = await request.json();
+
+  try {
+    console.log('Form data:', formData);
+    return {
+      statusCode: 200,
+      body: 'Form submitted successfully!',
+    };
+  } catch (error) {
+    console.error('Error submitting form:', error);
+    return {
+      statusCode: 500,
+      body: 'Failed to submit form',
+    };
   }
+}
